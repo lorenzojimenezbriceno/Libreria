c11ff95675ac25b84203cc51aca3d470d9a6a6ca
Lorenzo Jimenez <ljimenez@datasoftla.com>
2026-03-30 23:51:29 -0600

    Agregados datos de prueba

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Libreria
-Ejercicio de .NET University
+
+Ejercicio de .NET University, curso Foundational C# usando .NET y MVC
+
+# Creación del proyecto
 
 mkdir Libreria
 dotnet new globaljson
@@ -13,3 +16,10 @@ dotnet add package Microsoft.EntityFrameworkCore --version "9.0.4"
 dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version "9.0.4"
 dotnet add package Microsoft.EntityFrameworkCore.Tools --version "9.0.4"
 dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore --version "9.0.4"
+
+# Migración de la base de datos
+
+dotnet tool install --global dotnet-ef --version 9.0.4
+dotnet add package Microsoft.EntityFrameworkCore.Design --version "9.0.4"
+dotnet ef migrations add MigracionInicial
+dotnet ef database update 
--- src/AppStore/Program.cs
+++ src/AppStore/Program.cs
@@ -1,7 +1,30 @@
+using AppStore.Models.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Database conection
+builder.Services.AddDbContext<DatabaseContext>(opt => {
+    // Log SQL queries to the console
+    opt
+        .LogTo(Console.WriteLine, new [] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
+        .EnableSensitiveDataLogging();
+    // Initialize the database connection using the connection string from appsettings.json
+    opt
+        .UseSqlite(builder.Configuration.GetConnectionString("SqliteDatabase"));
+});
+
+// Add Identity services
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<DatabaseContext>()
+    .AddDefaultTokenProviders();
+
+// ------------------------------------------------------------------------
 
 var app = builder.Build();
 
@@ -13,17 +36,38 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Configurar la aplicacion.
 app.UseHttpsRedirection();
 app.UseRouting();
 
+// Agregar autenticacion y autorizacion.
+app.UseAuthentication();
 app.UseAuthorization();
 
+// Configurar las rutas y los activos estaticos.
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// Cargar los datos de prueba en la base de datos.
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<DatabaseContext>();
+        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        LoadDatabase.InsertarData(context, userManager, roleManager).Wait();
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error durante la carga de datos de prueba en la base de datos.");
+    }
+}
 
+// Ejecutar la aplicacion.
 app.Run();
